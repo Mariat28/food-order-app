32281b0d48498c4f4062cc8fb99c35b81dbe6f15
Mariat28 <mariatndagire28@gmail.com>
2022-06-18 17:41:53 +0300

    login page design update

--- src/components/login/LoginPage.js
+++ src/components/login/LoginPage.js
@@ -1,5 +1,8 @@
 import Logo from "../../assets/images/fast-food.png";
+import DiningIcon from "../../assets/images/dining.svg";
+
 import FloatingInput from "../ui/FloatingInput";
+import React from "react";
 const LoginPage = () => {
   const usernameChangeHandler =(username)=>{
     console.log('username', username);
@@ -8,30 +11,35 @@ const LoginPage = () => {
     console.log('password', password);
   }
   return (
-    <div className="flex justify-center items-center h-full">
-      <div className="bg-white shadow-sm rounded-md h-96 w-1/2 border">
-        <div className="flex flex-col items-center p-4 ">
-          <img src={Logo} alt="logo" className="w-14 mt-2" />
-          <span className="text-slate-500 mt-1 font-semibold font-sans">
-            satisfy your cravings!
-          </span>
-        </div>
-        <form className="flex flex-col gap-2 items-start p-4 w-full h-full  min-h-full">
-            <FloatingInput type="text" label="username" id="uname" onInputChange={usernameChangeHandler} />
-            <FloatingInput type="password" label="password" id="upass" onInputChange={passwordChangeHandler}/>
-            <div className="flex justify-between w-full">
-              <div className="flex items-center text-white">
-                <input type="checkbox" id="check" className="text-white  cursor-pointer accent-yellow-650 h-3 rounded-sm w-3"/>
-                <label htmlFor="check" className="text-sm ml-1 text-slate-900">Remember me</label>
+    <React.Fragment>
+      <div className="flex justify-center items-center h-full bg-red-50 p-3">
+        <div className="bg-white shadow-sm rounded-md h-96 w-full md:w-1/2 xl:w-2/6 border">
+          <div className="flex flex-col items-center p-4 ">
+            <img src={Logo} alt="logo" className="w-14 mt-2" />
+            <span className="text-slate-500 mt-1 font-semibold font-sans">
+              satisfy your cravings!
+            </span>
+          </div>
+          <form className="flex flex-col gap-2 items-start p-4 w-full h-full  min-h-full">
+              <FloatingInput type="text" label="username" id="uname" onInputChange={usernameChangeHandler} />
+              <FloatingInput type="password" label="password" id="upass" onInputChange={passwordChangeHandler}/>
+              <div className="flex justify-between w-full">
+                <div className="flex items-center text-white">
+                  <input type="checkbox" id="check" className="text-white  cursor-pointer accent-yellow-650 h-3 rounded-sm w-3"/>
+                  <label htmlFor="check" className="text-sm ml-1 text-slate-900">Remember me</label>
+                </div>
+                <div className="underline text-sm cursor-pointer text-slate-900 hover:text-yellow-650">Forgot password?</div>
               </div>
-              <div className="underline text-sm cursor-pointer text-slate-900 hover:text-yellow-650">Forgot password?</div>
-            </div>
-            <div className="w-full mt-2">
-              <button type="submit" className="bg-yellow-650/80 w-full p-2 rounded-sm text-white flex justify-center border hover:border-yellow-650 hover:text-yellow-650 hover:bg-white">Login</button>
-            </div>
-        </form>
+              <div className="w-full mt-2">
+                <button type="submit" className="bg-yellow-650/80 w-full p-2 rounded-sm text-white flex justify-center border hover:border-yellow-650 hover:text-yellow-650 hover:bg-white">Login</button>
+              </div>
+          </form>
+        </div>
+        <div className="absolute bottom-10 left-10  w-fit invisible md:visible">
+          <img src={DiningIcon} alt="dining-icon" className="lg:h-96 lg:w-72 h-48 w-48"/>
+        </div>
       </div>
-    </div>
+    </React.Fragment>
   );
 };
 export default LoginPage;
--- src/components/ui/FloatingInput.js
+++ src/components/ui/FloatingInput.js
@@ -12,14 +12,14 @@ const FloatingInput = (props) => {
         type={props.type}
         id={props.id}
         placeholder="Mariat Ndagire"
-        className="flex-1 text-base appearance-none bg-slate-50 rounded p-2.5 mt-1 w-full peer border border-slate-300 placeholder-transparent  shadow-sm focus:rounded-md focus:border-yellow-650 focus:outline-none focus:border-0 focus:border-b"
+        className="flex-1 appearance-none bg-slate-50 rounded p-2.5 mt-2 text-sm w-full peer border border-slate-300 placeholder-transparent  shadow-sm focus:rounded-md focus:border-yellow-650 focus:outline-none focus:border-0 focus:border-b"
         autoComplete="off"
         value={inputValue}
         onChange={InputChangeHandler}
       />
       <label
         htmlFor={props.id}
-        className=" ml-4 top-4 absolute  -translate-y-2 -translate-x-2 peer-focus:-translate-y-2 text-gray-400 text-xs duration-300 peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-7"
+        className=" ml-5 top-4 absolute  -translate-y-2 -translate-x-2 peer-focus:-translate-y-2 text-gray-400 text-xs duration-300 peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-7"
       >
         {props.label}
       </label>
